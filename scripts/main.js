f3b10309152bb9cfaedf4eaba98800d0c73a309b
Aishwary Gupta <guptashark@protonmail.com>
2021-10-03 14:01:27 -0400

    Working visible output of calculation
    We're able to output somewhere on the webpage,
    a calculation of what we did with the input.

--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,12 @@ function calc_solution() {
   var a_21 = parseInt(document.getElementById('input_a_21').value);
   var a_22 = parseInt(document.getElementById('input_a_22').value);
 
-  console.log(a_11);
-  console.log(a_12);
-  console.log(a_21);
-  console.log(a_22);
+  var x = a_11 + a_21;
+  var y = a_12 + a_22;
+
+  var out_x = document.getElementById('output_x');
+  var out_y = document.getElementById('output_y');
+
+  out_x.innerHTML = "x = " + x.toString();
+  out_y.innerHTML = "y = " + y.toString();
 }
