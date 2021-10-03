7c8bee33c870b305fed8de1927914c247ca165ba
Aishwary Gupta <guptashark@protonmail.com>
2021-10-03 13:16:47 -0400

    Function that is able to use textbox input

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
 alert("Hello World!");
+
+function calc_solution() {
+  var a_11 = parseInt(document.getElementById('input_a_11').value);
+  var a_12 = parseInt(document.getElementById('input_a_12').value);
+  var a_21 = parseInt(document.getElementById('input_a_21').value);
+  var a_22 = parseInt(document.getElementById('input_a_22').value);
+
+  console.log(a_11);
+  console.log(a_12);
+  console.log(a_21);
+  console.log(a_22);
+}
