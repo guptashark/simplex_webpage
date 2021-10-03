1af59346a2b56a91c72b410d4c29d2f764813971
Aishwary Gupta <guptashark@protonmail.com>
2021-10-03 14:32:28 -0400

    Function to calculate a 2x2 matrix inverse

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,21 @@
 alert("Hello World!");
 
+function calc_inverse(inv_mat, mat_A) {
+  inv_mat[0][0] = mat_A[1][1];
+  inv_mat[1][1] = mat_A[0][0];
+
+  inv_mat[0][1] = mat_A[0][1] * -1;
+  inv_mat[1][0] = mat_A[1][0] * -1;
+
+  // determinant
+  var det = mat_A[0][0] * mat_A[1][1] - mat_A[1][0] * mat_A[0][1];
+
+  inv_mat[0][0] = inv_mat[0][0] / det;
+  inv_mat[0][1] = inv_mat[0][1] / det;
+  inv_mat[1][0] = inv_mat[1][0] / det;
+  inv_mat[1][1] = inv_mat[1][1] / det;
+}
+
 function helper_fn(soln_arr, input_arr) {
   soln_arr[0] = input_arr[0][0] + input_arr[0][1];
   soln_arr[1] = input_arr[1][0] + input_arr[1][1];
@@ -12,6 +28,10 @@ function calc_solution() {
   matrix_A[0][1] = parseInt(document.getElementById('input_a_12').value);
   matrix_A[1][0] = parseInt(document.getElementById('input_a_21').value);
   matrix_A[1][1] = parseInt(document.getElementById('input_a_22').value);
+
+  inv_mat = [[0, 0], [0, 0]];
+  calc_inverse(inv_mat, matrix_A);
+  console.log(inv_mat);
 
   soln_arr = [0, 0];
   helper_fn(soln_arr, matrix_A);
