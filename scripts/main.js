5240bdf68e596ecf64e6ae0b77bebef197c9db5d
Aishwary Gupta <guptashark@protonmail.com>
2021-10-03 14:17:49 -0400

    Use a 2d array for matrices.

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,20 @@
 alert("Hello World!");
 
 function helper_fn(soln_arr, input_arr) {
-  soln_arr[0] = input_arr[0] + input_arr[1];
-  soln_arr[1] = input_arr[2] + input_arr[3];
+  soln_arr[0] = input_arr[0][0] + input_arr[0][1];
+  soln_arr[1] = input_arr[1][0] + input_arr[1][1];
 }
 
 function calc_solution() {
-  var a_11 = parseInt(document.getElementById('input_a_11').value);
-  var a_12 = parseInt(document.getElementById('input_a_12').value);
-  var a_21 = parseInt(document.getElementById('input_a_21').value);
-  var a_22 = parseInt(document.getElementById('input_a_22').value);
+
+  matrix_A = [[0, 0], [0, 0]];
+  matrix_A[0][0] = parseInt(document.getElementById('input_a_11').value);
+  matrix_A[0][1] = parseInt(document.getElementById('input_a_12').value);
+  matrix_A[1][0] = parseInt(document.getElementById('input_a_21').value);
+  matrix_A[1][1] = parseInt(document.getElementById('input_a_22').value);
 
   soln_arr = [0, 0];
-  input_arr = [a_11, a_12, a_21, a_22];
-  helper_fn(soln_arr, input_arr);
+  helper_fn(soln_arr, matrix_A);
 
   var out_x = document.getElementById('output_x');
   var out_y = document.getElementById('output_y');
